7b076fce0a30c9fc94412b86ff27e8966336869d
Jim Huang <hjijin.gz@gmail.com>
2022-06-24 16:17:02 +0800

    Testing Our JavaScript Functionality

--- test/system/product_test.rb
+++ test/system/product_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
   setup do
-    @product = products(:one)
+    @product = products(:ruby)
   end
 
   test "visiting the index" do
@@ -20,7 +20,7 @@ class ProductsTest < ApplicationSystemTestCase
     fill_in "Title", with: @product.title
     click_on "Create Product"
 
-    assert_text "Product was successfully created"
+    assert_text "Title has already been taken"
     click_on "Back"
   end
 
