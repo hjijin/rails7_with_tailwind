e1bd816bce421416a04995ab5a2234f2609c5021
Jim Huang <hjijin.gz@gmail.com>
2022-06-24 15:40:02 +0800

    Capturing an Order

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,13 +3,12 @@ class Cart < ApplicationRecord
 
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
-
     if current_item
       current_item.quantity += 1
     else
       current_item = line_items.build(product_id: product.id)
+      current_item.price = product.price
     end
-
     current_item
   end
 
